4719ef0aa6439344741d847ef2f58af1506e1718
ecrinkiilic <ecrinkiilic07@gmail.com>
2026-07-02 19:25:06 +0200

    Add files via upload

---
+++ Crypto-MVVM-Api&Json/Crypto-MVVM-Api&Json/Model/CryptoCurrency.swift
@@ -0,0 +1,14 @@
+//
+//  CryptoCurrency.swift
+//  Crypto-MVVM-Api&Json
+//
+//
+
+import Foundation
+
+struct CryptoCurrency: Decodable {
+    
+    let currency : String
+    let price : String
+    
+}
---
+++ Crypto-MVVM-Api&Json/Crypto-MVVM-Api&Json/Service/WebService.swift
@@ -0,0 +1,33 @@
+//
+//  WebService.swift
+//  Crypto-MVVM-Api&Json
+//
+//
+
+import Foundation
+
+class WebService {
+    
+    func downloadCurrencies(url: URL, completion: @escaping ([CryptoCurrency]?) -> ()) {
+        
+        
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                print(error.localizedDescription)
+                completion(nil)
+                
+            } else if let data = data {
+                let cryptoList = try? JSONDecoder().decode([CryptoCurrency].self, from: data)
+                print(cryptoList)
+                
+                if let cryptoList = cryptoList {
+                    completion(cryptoList)
+                }
+              
+            }
+        }.resume()
+        
+    }
+    
+}
